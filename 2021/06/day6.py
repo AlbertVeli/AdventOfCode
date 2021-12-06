dd7dd82f02a9951708a542212b678b8fb335d0d5
Albert Veli <albert.veli@gmail.com>
2021-12-06 11:56:48 +0100

    day6: improve speed by using deque

--- 2021/06/day6.py
+++ 2021/06/day6.py
@@ -3,32 +3,27 @@
 import sys
 sys.path.insert(0,'../')
 from aoc_input import *
+from collections import deque
 
 if len(sys.argv) != 2:
     print('Usage:', sys.argv[0], '<input.txt>')
     sys.exit(1)
 
 a = input_as_line_of_ints(sys.argv[1])
-fish = {}
-for i in range(9):
-    fish[i] = 0
+fish = deque([0] * 9)
 for i in a:
     fish[i] += 1
-
-org_fish = dict(fish)
 
 def go_fishing(fish, days):
     for i in range(days):
         new_fishes = fish[0]
-        for j in range(8):
-            fish[j] = fish[j + 1]
+        # rotate puts fish[0] -> fish[8]
+        fish.rotate(-1)
         fish[6] += new_fishes
-        fish[8] = new_fishes
-    return sum(fish.values())
+    return sum(fish)
 
 # Part 1
 print(go_fishing(fish, 80))
 
 # Part 2
-fish = dict(org_fish)
-print(go_fishing(fish, 256))
+print(go_fishing(fish, 256 - 80))
