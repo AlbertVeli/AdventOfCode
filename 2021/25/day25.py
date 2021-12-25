4f34fea60bf80ccd99c458c88745cdbc9648023f
Albert Veli <albert.veli@gmail.com>
2021-12-25 07:00:05 +0100

    2021 day 25

---
+++ 2021/25/day25.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+
+if len(sys.argv) != 2:
+    print('Usage:', sys.argv[0], '<input.txt>')
+    sys.exit(1)
+
+a = []
+for line in open(sys.argv[1]):
+    a.append(list(line.rstrip()))
+a = np.array(a)
+sy, sx = a.shape
+
+# Could probably move without copying a
+# but logic is simpler if keeping a copy
+def move(a):
+    moved = False
+    # Move right
+    a_org = np.array(a)
+    for y in range(sy):
+        ny = y
+        for x in range(sx):
+            if a_org[y][x] != '>':
+                continue
+            if x == sx - 1:
+                nx = 0
+            else:
+                nx = x + 1
+            if a_org[ny][nx] == '.':
+                a[ny][nx] = '>'
+                a[y][x] = '.'
+                moved = True
+    # Move down
+    a_org = np.array(a)
+    for y in range(sy):
+        if y == sy - 1:
+            ny = 0
+        else:
+            ny = y + 1
+        for x in range(sx):
+            nx = x
+            if a_org[y][x] != 'v':
+                continue
+            if a_org[ny][nx] == '.':
+                a[ny][nx] = 'v'
+                a[y][x] = '.'
+                moved = True
+    return moved
+
+moves = 0
+while move(a):
+    moves += 1
+
+#print(a)
+print(moves + 1)
