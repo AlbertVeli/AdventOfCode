9f645c804998a412f6e9f5eda79eb5fac362fa1d
Albert Veli <albert.veli@gmail.com>
2022-12-01 07:27:16 +0100

    Change source to . in get_input.sh
    Otherwise it won't work in WSL

--- get_day.sh
+++ get_day.sh
@@ -27,6 +27,6 @@ day=$1
 # Put your own session cookie and year in cookie.txt
 # year=2019
 # cookie='cookie: session=1234abcd..'
-source ${DIR}/cookie.txt
+. ${DIR}/cookie.txt
 
 curl "https://adventofcode.com/${year}/day/${day}" -H "$cookie" --compressed
