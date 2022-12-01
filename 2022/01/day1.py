e5614a2369aa5dead61a0987de1c35bd6736f487
Albert Veli <albert.veli@gmail.com>
2022-12-01 12:10:40 +0100

    day1 remove extra parethesis

--- 2022/01/day1.py
+++ 2022/01/day1.py
@@ -11,11 +11,10 @@ totals = []
 # Each user is separated by empty line, strip last newline
 for user in open(sys.argv[1]).read().rstrip().split('\n\n'):
     # Each value for one user separated by \n, sum them up
-    totals.append((sum(map(int, user.split('\n')))))
+    totals.append(sum(map(int, user.split('\n'))))
 
 totals.sort(reverse = True)
-totals = totals[:3]
 
 print('Part 1:', totals[0])
 #print(totals)
-print('Part 2:', sum(totals))
+print('Part 2:', sum(totals[:3]))
