7a8c9af275f1cde34c81f77b3cb204d27fd3fbb4
Albert Veli <albert.veli@gmail.com>
2023-12-01 08:17:27 +0100

    Cleanup day1

--- 2023/01/day1.py
+++ 2023/01/day1.py
@@ -9,19 +9,18 @@ a = aoc.lines_of_ints(sys.argv[1])
 sm = 0
 for l in a:
     s = ''.join(map(str, l))
-    s2 = s[0] + s[-1]
-    sm += int(s2)
+    sm += int(s[0] + s[-1])
 
 print('Part 1:', sm)
 
 # part 2
-numdic = {'one' : 1, 'two' : 2, 'three' : 3, 'four' : 4, 'five' : 5, 'six' : 6, 'seven' : 7, 'eight' : 8, 'nine' : 9}
+numdic = {'one' : '1', 'two' : '2', 'three' : '3', 'four' : '4', 'five' : '5', 'six' : '6', 'seven' : '7', 'eight' : '8', 'nine' : '9'}
 keys = numdic.keys()
-nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+nums = numdic.values()
 
 def replace_subnums(s):
     """
-    Replace subscrings that are in numdic. The keys may overlap.
+    Replace substrings that are in numdic. The keys may overlap.
     """
     result = []
     pos = 0
@@ -32,7 +31,7 @@ def replace_subnums(s):
                 m = key
                 break
         if m:
-            result.append(str(numdic[m]))
+            result.append(numdic[m])
         elif s[pos] in nums:
             result.append(s[pos])
         pos += 1
