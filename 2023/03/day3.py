5b2bdabc29e7ea7688a79512311d1ef9f970fb24
Albert Veli <albert.veli@gmail.com>
2023-12-03 19:37:10 +0100

    refactor day3
    Use common function to return list of adjacent coords
    that is used in both part 1 and part 2.

--- 2023/03/day3.py
+++ 2023/03/day3.py
@@ -5,32 +5,31 @@ sys.path.append('../..')
 import aoc
 import re
 
-lines = aoc.char_matrix(sys.argv[1])
+lines = aoc.lines(sys.argv[1])
 for i, line in enumerate(lines):
     # add border of '.' around it
-    lines[i] = '.' + ''.join(line) + '.'
+    lines[i] = '.' + line + '.'
 # first and last line of '.'
 m = ['.' * len(lines[0])] + lines + ['.' * len(lines[0])]
 
-def adj_dots(x, y, x2):
-    # TODO: Create list of coordinates
-    # to check instead. Break this out
-    # into a separate function and call
-    # it from adj_dots and adj_nums.
-    # Line above
-    yy = y - 1
-    for c in m[yy][x - 1 : x2 + 1]:
-        if c != '.':
-            return False
-    # Left
-    if m[y][x - 1] != '.' or m[y][x2] != '.':
-        return False
-    # Line below
-    yy = y + 1
-    for c in m[yy][x - 1 : x2 + 1]:
-        if c != '.':
-            return False
+def adj_coords(x, y, x2):
+    coords = []
+    # above
+    for xx in range(x - 1, x2 + 1):
+        coords.append((xx, y - 1))
+    # left, right
+    coords.append((x - 1, y))
+    coords.append((x2, y))
+    # below
+    for xx in range(x - 1, x2 + 1):
+        coords.append((xx, y + 1))
+    return coords
 
+# return True if all adjacent coordinates are '.'
+def adj_dots(x, y, x2):
+    for xx, yy in adj_coords(x, y, x2):
+        if m[yy][xx] != '.':
+            return False
     return True
 
 # Find number tuple at x, y in nums
@@ -42,31 +41,8 @@ def num(x, y):
 
 # Find exactly 2 adjacent numbers
 def adj_nums(x, y):
-    # TODO: Make list of coordinates to
-    # check and just do the
-    # adjnums.append() in one place
     adjnums = []
-    # Line above
-    yy = y - 1
-    for xx in range(x - 1, x + 2):
-        if m[yy][xx].isdigit():
-            n = num(xx, yy)
-            if not n in adjnums:
-                adjnums.append(n)
-    # Left
-    if m[y][x - 1].isdigit():
-        n = num(x - 1, y)
-        if not n in adjnums:
-            adjnums.append(n)
-    # Right
-    if m[y][x + 1].isdigit():
-        n = num(x + 1, y)
-        if not n in adjnums:
-            adjnums.append(n)
-
-    # Line below
-    yy = y + 1
-    for xx in range(x - 1, x + 2):
+    for xx, yy in adj_coords(x, y, x + 1):
         if m[yy][xx].isdigit():
             n = num(xx, yy)
             if not n in adjnums:
