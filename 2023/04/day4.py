b67b01b28098662657ec949cb580d5fc2721ec1c
Albert Veli <albert.veli@gmail.com>
2023-12-04 12:57:15 +0100

    refactor day4 part 2

--- 2023/04/day4.py
+++ 2023/04/day4.py
@@ -27,15 +27,10 @@ for wins, mynums in scratchcard:
 print('Part 1:', sm)
 
 # part 2
-# TODO: refactor this to use a plain list instead
-m = {n: 1 for n in range(1, len(cards) + 1)}
+l = [1] *len(cards)
 for i, matches in enumerate(cards):
-    n = i + 1
-    mul = m[n]
-    for num in range(n + 1, n + matches + 1):
-        if num in m:
-            m[num] += mul
-        else:
-            m[num] = mul
+    mul = l[i]
+    for n in range(i + 1, i + matches + 1):
+            l[n] += mul
 
-print('Part 2:', sum(m.values()))
+print('Part 2:', sum(l))
