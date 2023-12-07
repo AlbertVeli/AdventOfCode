5c5bf15d9b47202fa9f3525250ca5233f26ee90a
Albert Veli <albert.veli@gmail.com>
2023-12-07 20:02:26 +0100

    refactor 2017 day 1

--- 2017/01/day1.py
+++ 2017/01/day1.py
@@ -6,21 +6,16 @@ import aoc
 
 line = aoc.input_string(sys.argv[1])
 linelen = len(line)
-halflen = linelen // 2
-matches = []
-for i in range(len(line)):
-    c1 = line[i]
-    i2 = (i + 1) % linelen
-    c2 = line[i2]
-    if c1 == c2:
-        matches.append(int(c1))
-print('Part 1:', sum(matches))
 
-matches = []
-for i in range(len(line)):
-    c1 = line[i]
-    i2 = (i + halflen) % linelen
-    c2 = line[i2]
-    if c1 == c2:
-        matches.append(int(c1))
-print('Part 2:', sum(matches))
+def run_part(offset):
+    matches = []
+    for i in range(linelen):
+        c1 = line[i]
+        i2 = (i + offset) % linelen
+        c2 = line[i2]
+        if c1 == c2:
+            matches.append(int(c1))
+    return sum(matches)
+
+print('Part 1:', run_part(1))
+print('Part 2:', run_part(linelen // 2))
