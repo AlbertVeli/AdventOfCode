4b618580a0f8a6307a2268eec1fdfe9989ea5c18
Albert Veli <albert.veli@gmail.com>
2023-12-13 20:51:20 +0100

    Refactor day 13 some more

--- 2023/13/day13.py
+++ 2023/13/day13.py
@@ -7,10 +7,10 @@ import numpy as np
 
 patterns = list(map(str.splitlines, aoc.input_string(sys.argv[1]).split('\n\n')))
 
-def is_reflective(pattern, i):
+def is_reflective(pattern, i, part2):
     height = len(pattern)
-    # smallest number of rows, upper or lower
-    # part of pattern split at row i
+    # smallest number of rows when pattern
+    # is split at row i
     n_rows = sorted([i, height - i])[0]
     lower = pattern[i - n_rows : i]
     upper = pattern[i : i + n_rows]
@@ -18,41 +18,34 @@ def is_reflective(pattern, i):
     upper = upper[::-1]
     # This is the only part that differs between
     # part 1 and 2
-    if part1:
-        diff_wanted = 0
-    else:
+    if part2:
         diff_wanted = 1
+    else:
+        diff_wanted = 0
     if np.sum(upper != lower) == diff_wanted:
         return True
     return False
 
-def find_reflection(pattern):
+def find_reflection(pattern, part2):
 
     height = len(pattern)
 
     for i in range(1, height):
-        if is_reflective(pattern, i):
+        if is_reflective(pattern, i, part2):
             return i
     return 0
 
-def score_reflection(pattern):
+def score_reflection(pattern, part2 = False):
     pattern = np.array([list(s) for s in pattern])
-    n = find_reflection(pattern) * 100
+    n = find_reflection(pattern, part2) * 100
     if n > 0:
         return n
-    return find_reflection(pattern.T)
+    return find_reflection(pattern.T, part2)
 
-part1 = True
-vals = []
+vals1 = []
+vals2 = []
 for pattern in patterns:
-    n = score_reflection(pattern)
-    vals.append(n)
-print('Part 1:', sum(vals))
-
-# part2
-part1 = False
-vals = []
-for pattern in patterns:
-    n = score_reflection(pattern)
-    vals.append(n)
-print('Part 2:', sum(vals))
+    vals1.append(score_reflection(pattern))
+    vals2.append(score_reflection(pattern, part2 = True))
+print('Part 1:', sum(vals1))
+print('Part 2:', sum(vals2))
