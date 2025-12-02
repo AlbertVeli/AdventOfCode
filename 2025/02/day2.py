06b56b648c4fe949ca1f3024e8a66ba74e39e445
Albert Veli <albert.veli@gmail.com>
2025-12-02 06:37:11 +0100

    2025 day 2

---
+++ 2025/02/day2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import sys
+
+def is_invalid_id(n):
+    s = str(n)
+    if len(s) % 2 != 0:
+        return False
+    half = len(s) // 2
+    return s[:half] == s[half:]
+
+def is_invalid_id_2(n):
+    s = str(n)
+    L = len(s)
+
+    for period_len in range(1, L // 2 + 1):
+        if L % period_len != 0:
+            # not divisible
+            continue
+
+        repeats = L // period_len
+        if repeats < 2:
+            continue
+
+        pattern = s[:period_len]
+        if pattern * repeats == s:
+            return True
+
+    return False
+
+filename = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+with open(filename, 'r') as f:
+    line = f.read().strip()
+
+ranges = []
+for part in line.split(','):
+    a, b = part.split('-')
+    ranges.append((int(a), int(b)))
+
+output = 0
+for r1, r2 in ranges:
+    for n in range(r1, r2 + 1):
+        if is_invalid_id(n):
+            output += n
+
+print('Part 1:', output)
+
+output = 0
+for r1, r2 in ranges:
+    for n in range(r1, r2 + 1):
+        if is_invalid_id_2(n):
+            output += n
+
+print('Part 2:', output)
